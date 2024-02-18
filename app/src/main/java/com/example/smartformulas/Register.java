832d421b6d188736588109b24dfb84b6cc028813
Alex <alex.dilakyan231221@gmail.com>
2024-02-18 07:58:47 +0400

    Smart Formulas App 1.02

--- app/src/main/java/com/example/smartformulas/Register.java
+++ app/src/main/java/com/example/smartformulas/Register.java
@@ -5,6 +5,7 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.View;
@@ -56,7 +57,6 @@ public class  Register extends AppCompatActivity {
                                             finish();
                                         }
                                     });
-
         buttonReg.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
